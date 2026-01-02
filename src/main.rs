9f218f7365756053a069716fcbcd1819680370f8
chintan-27 <chintan.acharya27@gmail.com>
2026-01-01 20:08:51 -0500

    feat(app): bootstrap gui app and spawn sensor thread

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
 mod app;
+mod bellows;
+mod keymap;
+mod audio;
+mod sensor;
 
 fn main() -> eframe::Result<()> {
+    // Create a standard (non-async) channel to send sensor messages to the GUI.
+    let (tx, rx) = std::sync::mpsc::channel::<sensor::SensorMsg>();
+
+    // Start the sensor in a background thread.
+    // It will try to connect and stream angle samples.
+    // If the device isn't available, you'll see the error in the UI.
+    let _sensor_thread = sensor::spawn_sensor_thread(60.0, tx);
+
     let options = eframe::NativeOptions::default();
 
     eframe::run_native(
         "Harmonium",
         options,
-        Box::new(|_cc| {
-            // Create our app component here
-            let harmonium = app::HarmoniumApp::new();
+        Box::new(move |_cc| {
+            let harmonium = app::HarmoniumApp::new(rx);
 
-            // eframe expects something that implements eframe::App
             Ok(Box::new(EguiAppWrapper { inner: harmonium }))
         }),
     )
